b539d5cd5e4beb6645a90769cd20c38185eb4353
Simeon <mrayendi1@gmail.com>
2024-04-24 12:54:41 -0700

    'update'

--- client/phraims/src/app/login/page.tsx
+++ client/phraims/src/app/login/page.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-export default function home (){
+interface FormData  {
+    username: string;
+    password: string;
+    id: string;
+
+}
+
+
+const  home: React.FC = () => {
+
+    const [ formData, setFormData ] = useState<FormData>({
+        username: '',
+        password: '',
+        id: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    }
+
+    const handleSubmit = () => {
+        console.log('It was submitte')
+
+
+    }
 
     return(
-        <h1>Hello World</h1>
+       <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username:</label> 
+        <input
+            value={formData.username}
+            type='text'
+            id='username'
+            name='username'
+            onChange={handleChange}
+        />
+       </form>
     )
 }
+exports.default = home;
